46fbbed2d1a0406310afe16f32b456cc4bc02fa4
karinatech <karinaminty@gmail.com>
2021-12-22 14:29:08 -0800

    challenges day 1

---
+++ src/main/java/org/example/PhoneBook.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PhoneBook {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        Map<String,Integer> book=new HashMap<>();
+
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+                book.put(name,phone);
+        }
+        while(in.hasNext()) {
+            String s=in.nextLine();
+      if(book.containsKey(s)){
+          System.out.println(s+" "+book.get(s));
+      }else{
+          System.out.println("Not found");
+      }
+
+        }
+    }
+}
