4883f3ec4b5e8c802a96b189602fb3cd4a2c6c42
karinatech <karinaminty@gmail.com>
2021-12-22 11:37:30 -0800

    challenges day 1

---
+++ src/main/java/org/example/APOD.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class APOD {
+    public final String copyright;
+    public final String date;
+    public final String explanation;
+    public final String mediaType;
+    public final String serviceVersion;
+    public final String title;
+    public final String url;
+
+    public APOD(@JsonProperty("copyright") String copyright,
+                @JsonProperty("date") String date,
+                @JsonProperty("explanation") String explanation,
+                @JsonProperty("media_type") String mediaType,
+                @JsonProperty("service_version") String serviceVersion,
+                @JsonProperty("title") String title,
+                @JsonProperty("url") String url) {
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
+        this.title = title;
+        this.url = url;
+    }
+}
